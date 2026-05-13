c00ec97ec1ada91a24d4dbfe2172c3f8d470e849
dskrhkrdskrhkr-cyber <dskrhkrdskrhkr@gmail.com>
2026-05-13 13:59:44 +0300

    الملفات الأولية لمشروع MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+n=0
+indeex -0
+def on_button_pressed_a()
+global n
+n+=1
+basic .show_nuber(n)
+input .on_button_pressed(button.on_button_pressed_a)
+def on_button_pressd_b():
+global n 
+n-=1
+basic.show_number(n)
+input .on_Button_pressed_ab():
+def on_button_pressed_ab():
+global indeex
+while index <=n:
+basic .show_Number(index**2)
+index +=1
+
+input.on_Button_Pressed(Button.AB, on_button_pressed_ab)
